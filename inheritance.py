236095d4e44eea6e831a98f51ef9347cd5ec8040
edely <edely.gomes@gmail.com>
2017-08-27 07:07:48 -0300

    final

--- inheritance.py
+++ inheritance.py
@@ -5,6 +5,10 @@ class Parent():
 		self.last_name = last_name
 		self.eye_color = eye_color
 
+	def show_info(self):
+		print("Last Name: " + self.last_name)
+		print("Eye Color: " + self.eye_color)
+
 
 class Child(Parent):
 
@@ -13,9 +17,17 @@ class Child(Parent):
 		Parent.__init__(self, last_name, eye_color)
 		self.number_of_toys = number_of_toys
 		
-#billy_cyrus = Parent("Cyrus", "blue")		
-#print(billy_cyrus.last_name)
+	def show_info(self):
+		print("Last Name: " + self.last_name)
+		print("Eye Color: " + self.eye_color)
+		print("Number of Toyes: " + str(self.number_of_toys))
+		#Parent.show_info()
 
+
+billy_cyrus = Parent("Cyrus", "blue")		
+#print(billy_cyrus.last_name)
+#billy_cyrus.show_info()
 miley_cyrus = Child("Cyrus", "blue", 5)	
-print(miley_cyrus.last_name)
-print(miley_cyrus.eye_color)
+miley_cyrus.show_info()
+#print(miley_cyrus.last_name)
+#print(miley_cyrus.eye_color)
